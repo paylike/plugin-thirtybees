d4ef7784fb74129c58db9b1277822ee062cc302c
LucianDex <remindfwd@gmail.com>
2022-04-05 09:52:10 +0300

    improve tests

--- cypress/support/test_helper.js
+++ cypress/support/test_helper.js
@@ -20,25 +20,13 @@ export let PaylikeTestHelper = {
      * @return {Number}
      */
      filterAndGetAmountInMinor($unfilteredAmount, currency) {
-        /** Replace any character except numbers, commas, points */
-        var filtered = ($unfilteredAmount.text()).replace(/[^0-9,.]/g, '')
-        var matchPointFirst = filtered.match(/\..*,/g);
-        var matchCommaFirst = filtered.match(/,.*\./g);
-
-        if (matchPointFirst) {
-            var amountAsText = (filtered.replace('.', '')).replace(',', '.');
-        } else if (matchCommaFirst) {
-            var amountAsText = filtered.replace(',', '');
-        } else {
-            var amountAsText = filtered.replace(',', '.');
-        }
-
-        var formattedAmount = parseFloat(amountAsText);
+        var formattedAmount = this.filterAndGetAmountInMajorUnit($unfilteredAmount);
 
         /** Get multiplier based on currency code. */
         var multiplier = PaylikeCurrencies.get_paylike_currency_multiplier(currency);
 
-        return formattedAmount * multiplier;
+        // return formattedAmount * multiplier;
+        return Math.ceil(Math.round(formattedAmount * multiplier));
     },
 
     /**
@@ -46,13 +34,12 @@ export let PaylikeTestHelper = {
      * Get it in currency major unit
      *
      * @param {Object} $unfilteredAmount
-     * @param {String} currency
      *
      * @return {Number}
      */
-     filterAndGetAmountInMajorUnit($unfilteredAmount, currency) {
+     filterAndGetAmountInMajorUnit($unfilteredAmount) {
         /** Replace any character except numbers, commas, points */
-        var filtered = ($unfilteredAmount.text()).replace(/[^0-9,.]/g, '')
+        var filtered = ($unfilteredAmount.text()).replace(/[^0-9,.][a-z.]*/g, '')
         var matchPointFirst = filtered.match(/\..*,/g);
         var matchCommaFirst = filtered.match(/,.*\./g);
 
